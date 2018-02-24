cee6716cc2fd6647111b35128c2618a40d23586e
Kevin <github.edm@m2k.com.tw>
2018-02-24 16:18:20 +0800

    加入專案檔案。

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello World! R2");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new Class1();
+            o.Run();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
